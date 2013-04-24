d857de4a70ae40748f99bdcd1e908985cd657330
alberto <alberto@192.168.1.11>
2013-04-24 20:10:22 +0200

    a

---
+++ metagit/src/albertocasas123pckg/albertocasas123.java
@@ -0,0 +1,13 @@
+package albertocasas123pckg;
+
+public class albertocasas123 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

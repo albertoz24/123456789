dda6e1176c3012c9457bc8079c53bf9838bd52f3
alberto <alberto@192.168.1.11>
2013-04-24 20:52:28 +0200

    asd

--- metagit/src/albertocasas123pckg/albertocasas123.java
+++ metagit/src/albertocasas123pckg/albertocasas123.java
@@ -6,7 +6,8 @@ public class albertocasas123 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+		System.out.println("hola mundo");
 
 	}
 
